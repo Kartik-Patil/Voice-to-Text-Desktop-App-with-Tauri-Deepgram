fedcd281f0083e66a718a0d70eddc31f6e031963
Kartik-Patil <kartikpatilhrg@gmail.com>
2025-12-26 19:24:03 +0530

    Polishing Missing Functions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,6 @@
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_clipboard_manager::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
